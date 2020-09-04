1320c97b4eb14e4488a2a86b1dfdd389f4c88aa4
macunaima <gcgamer46@gmail.com>
2020-09-04 14:05:05 -0300

    Showing messages on index

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,18 +25,9 @@ class MessagesController < ApplicationController
   # POST /messages.json
   def create
     @message = Message.new(message_params)
-    
     @message.user = current_user
-
-    respond_to do |format|
-      if @message.save
-        format.html { redirect_to @message, notice: 'Message was successfully created.' }
-        format.json { render :show, status: :created, location: @message }
-      else
-        format.html { render :new }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
+    @message.save
+    redirect_to request.referer
   end
 
   # PATCH/PUT /messages/1

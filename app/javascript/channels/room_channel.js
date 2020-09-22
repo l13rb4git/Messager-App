d27282d2dd95f7903cf4df068785e9ba8c8aaf97
macunaima <gcgamer46@gmail.com>
2020-09-22 15:01:50 -0300

    Create channel to room

---
+++ app/javascript/channels/room_channel.js
@@ -0,0 +1,15 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1 }, {
+  connected() {
+      console.log("connected!")
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+      console.log(data)
+  }
+});

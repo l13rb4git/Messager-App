7e520861c6f027ee39194bbc0c36b22502afe6d1
macunaima <gcgamer46@gmail.com>
2020-09-22 15:10:50 -0300

    Create Job to catch the message html in the background

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,9 +27,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.user = current_user
     @message.save
-
-    html = render(partial: "messages/message", locals: { message: @message })
-    ActionCable.server.broadcast "room_channel_#{@message.room_id}", html: html
+    SendMessageJob.perform_later(@message)
   end
 
   # PATCH/PUT /messages/1

5d361317ae9099b29c233e3f83d203c3b6e31dbc
macunaima <gcgamer46@gmail.com>
2020-09-24 10:19:12 -0300

    making possible to the user switching between rooms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
 
   def current_user
-    return @current_user if @current_user.present?
+    return User.find(session[:user_id]) if @current_user.present?
 
     if session[:user_id].present?
       @current_user = User.find(session[:user_id])
--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -1,27 +1,34 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create({ channel: "RoomChannel", room_id: 1 }, {
-  connected() {
-      console.log("connected!")
-  },
+document.addEventListener('turbolinks:load', () => {
+    const current_room_element = document.getElementById('current_room_id');
+    const current_room_id = current_room_element.getAttribute('data-room-id');
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    consumer.subscriptions.create({ channel: "RoomChannel", room_id: current_room_id }, {
+      connected() {
+          console.log("connected!")
+      },
 
-  received(data) {
-      console.log(data)
-      const current_user_element = document.getElementById('current_user_id');
-      const current_user_id = Number(current_user_element.getAttribute('data-user-id'));
-      const messageContainer = document.getElementById('messages')
-      var html = new DOMParser().parseFromString('', 'text/xml');
-      html.getElementsByTagName('html')[0].innerHTML = data.html
+      disconnected() {
+        // Called when the subscription has been terminated by the server
+      },
 
-      if (current_user_id != data.message.user_id) {
-          html.getElementById("message").classList.remove('me');
-      } else {
-          html.getElementById("message").classList.add('me');
+      received(data) {
+          console.log(data)
+          const messageContainer = document.getElementById('messages')
+
+          var html = new DOMParser().parseFromString('', 'text/xml');
+          html.getElementsByTagName('html')[0].innerHTML = data.html
+
+          const current_user_element = document.getElementById('current_user_id');
+          const current_user_id = current_user_element.getAttribute('data-user-id');
+
+          if (current_user_id != data.message.user_id) {
+              html.getElementById("message").classList.remove('me');
+          } else {
+              html.getElementById("message").classList.add('me');
+          }
+          messageContainer.innerHTML = messageContainer.innerHTML + html.getElementsByTagName('html')[0].innerHTML
       }
-      messageContainer.innerHTML = messageContainer.innerHTML + html.getElementsByTagName('html')[0].innerHTML
-  }
-});
+    });
+})
